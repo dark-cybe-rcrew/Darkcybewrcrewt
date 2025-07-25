310d8767a128e2a28db37ef89b5490b323211785
dark-cybe-rcrew <chaminduransika03@gmail.com>
2025-07-25 18:58:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "CHAMA-MD=tV5yTSLL#4CIL4zbRweTDfSjaarcKx_BlOIXHpr-7cHZ3TICkXBE",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
